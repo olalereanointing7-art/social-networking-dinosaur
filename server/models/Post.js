6ef97e6033cbda605b592b0875f194014bc3542b
Anointing Kolapo Olalere <olalereanointing7@gmail.com>
2026-08-23 18:34:55 +0100

    Add Post model

---
+++ server/models/Post.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  image: String,
+  video: String,
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ],
+  shares: {
+    type: Number,
+    default: 0
+  },
+  views: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
